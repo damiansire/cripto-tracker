cad401ff6b0f808000a1e6f4ca230473b273b6c3
Damian Sire <damiansire@gmail.com>
2024-07-29 04:14:08 -0300

    feat: add service for handle csv with ; separator

--- src/app/pages/continuous-buying-selling/csv-data.service.ts
+++ src/app/pages/continuous-buying-selling/csv-data.service.ts
@@ -23,36 +23,32 @@ export class CsvDataService {
   getEthereumData(): Observable<EthereumData[]> {
     return this.http
       .get('assets/criptos-history/ethereum.csv', { responseType: 'text' })
-      .pipe(tap(console.log))
       .pipe(map((csvData) => this.parseCsv(csvData)));
   }
 
   private parseCsv(csvData: string): EthereumData[] {
-    const lines = csvData.split('\n');
-    const headers = lines[0].split(',');
-
-    return lines.slice(1).map((line) => {
-      const values = line.split(',');
-      return headers.reduce((obj: any, header, index) => {
-        switch (header) {
-          case 'timeOpen':
-          case 'timeClose':
-          case 'timeHigh':
-          case 'timeLow':
-            obj[header] = new Date(parseInt(values[index], 10));
-            break;
-          case 'priceOpen':
-          case 'priceHigh':
-          case 'priceLow':
-          case 'priceClose':
-          case 'volume':
-            obj[header] = parseFloat(values[index].replace(',', '.'));
-            break;
-          default:
-            obj[header] = values[index];
-        }
-        return obj;
-      }, {}) as EthereumData;
-    });
+    const rows: EthereumData[] = [];
+    const lines = csvData.split('\r\n');
+    const headers = lines[0].split(';');
+    for (let index = 1; index < lines.length - 1; index++) {
+      const line = lines[index];
+      const cleanLine = line.replace('\n', '');
+      const values = cleanLine.split(';');
+      if (values.length != headers.length) {
+        const row = {
+          timeOpen: new Date(parseInt(values[0], 10)),
+          timeClose: new Date(parseInt(values[1], 10)),
+          timeHigh: new Date(parseInt(values[2], 10)),
+          timeLow: new Date(parseInt(values[3], 10)),
+          priceOpen: parseFloat(values[4].replace(',', '.')),
+          priceHigh: parseFloat(values[5].replace(',', '.')),
+          priceLow: parseFloat(values[6].replace(',', '.')),
+          priceClose: parseFloat(values[7].replace(',', '.')),
+          volume: parseFloat(values[8].replace(',', '.')),
+        } as EthereumData;
+        rows.push(row);
+      }
+    }
+    return rows;
   }
 }
