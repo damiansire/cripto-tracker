87d7c2351668252c4819db9735ce4ab09e4f1230
Damian Sire <damiansire@gmail.com>
2024-07-29 03:27:09 -0300

    structure: add base structure for transaction tables

---
+++ src/app/pages/continuous-buying-selling/components/transaction-tables/transaction-tables.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-tables',
+  standalone: true,
+  imports: [],
+  templateUrl: './transaction-tables.component.html',
+  styleUrl: './transaction-tables.component.css'
+})
+export class TransactionTablesComponent {
+
+}
