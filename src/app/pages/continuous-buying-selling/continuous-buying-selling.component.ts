25b86c71ed0f38ecb42efc73cd66ba9a417b73fe
Damian Sire <damiansire@gmail.com>
2024-07-30 00:35:51 -0300

    feat: add simulation for buy/sell strategy

--- src/app/pages/continuous-buying-selling/continuous-buying-selling.component.ts
+++ src/app/pages/continuous-buying-selling/continuous-buying-selling.component.ts
@@ -1,11 +1,15 @@
-import { Component, inject, Signal, signal } from '@angular/core';
+import { Component, computed, inject, Signal, signal } from '@angular/core';
 import { EthereumData, CsvDataService } from './csv-data.service';
 import { CommonModule } from '@angular/common';
+import { TableModule } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
+
+type OrderStatus = 'pendingToBuy' | 'pendingToSell';
 
 interface Order {
   sellPrice: number;
   buyPrice: number;
-  isPending: boolean;
+  status: OrderStatus;
 }
 
 interface ClosedPosition {
@@ -14,19 +18,20 @@ interface ClosedPosition {
   profit: number;
 }
 
-interface SellOrder {
-  price: number;
-}
-
-interface Operation {
-  action: 'buy' | 'sell';
-  price: number;
+interface LastData {
+  timeHigh: Date;
+  timeLow: Date;
+  priceHigh: number;
+  priceLow: number;
+  priceClose: number;
+  priceOpen: number;
+  timeOpen: Date;
 }
 
 @Component({
   selector: 'app-continuous-buying-selling',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TableModule, TagModule],
   providers: [CsvDataService],
   templateUrl: './continuous-buying-selling.component.html',
   styleUrl: './continuous-buying-selling.component.css',
@@ -34,117 +39,172 @@ interface Operation {
 export class ContinuousBuyingSellingComponent {
   private csvDataService = inject(CsvDataService);
   ethereumData = signal<EthereumData[]>([]);
-  step = 132;
+  step = 1;
   money = 2600;
+  intervalForBuy = 50;
   orders = signal<Order[]>([]);
-  pendingSellOrders = signal<SellOrder[]>([]);
-  stock = signal<Operation[]>([]);
   historicalClosedPosition = signal<ClosedPosition[]>([]);
+  profit = computed(() => {
+    return this.historicalClosedPosition().reduce((acc, current) => {
+      return acc + current.profit;
+    }, 0);
+  });
 
-  portfolio = [];
-  lastData = { priceHigh: 0, priceLow: 0 };
+  lastData = signal<LastData>({
+    priceHigh: 0,
+    priceLow: 0,
+    priceClose: 0,
+    priceOpen: 0,
+    timeHigh: new Date(),
+    timeLow: new Date(),
+    timeOpen: new Date(),
+  });
   ngOnInit() {
     this.csvDataService.getEthereumData().subscribe((data) => {
       this.ethereumData.set(data);
-      this.lastData = {
-        priceHigh: this.ethereumData()[this.step].priceHigh,
-        priceLow: this.ethereumData()[this.step].priceLow,
-      };
+      this.lastData.set(this.getLastDataFromStep(this.step));
+      for (let index = 1; index < this.ethereumData().length - 1; index++) {
+        this.nextStep();
+      }
     });
     //Elijo comprar del 1000 al 3600
     for (let index = 1; index <= 36; index++) {
       this.orders.update((x) => [
         ...x,
         {
-          sellPrice: 1000 + index * 100 + 100,
-          buyPrice: 1000 + index * 100,
-          isPending: true,
+          sellPrice: 1000 + index * this.intervalForBuy + this.intervalForBuy,
+          buyPrice: 1000 + index * this.intervalForBuy,
+          status: 'pendingToBuy',
         },
       ]);
     }
   }
+  sortOrdersByProximity(orders: Order[], referencePrice: number) {
+    return orders.slice().sort((a, b) => {
+      const distanceA =
+        Math.abs(a.buyPrice - referencePrice) +
+        Math.abs(a.sellPrice - referencePrice);
+      const distanceB =
+        Math.abs(b.buyPrice - referencePrice) +
+        Math.abs(b.sellPrice - referencePrice);
+      return distanceA - distanceB;
+    });
+  }
   nextStep() {
     this.step++;
-    const newData = {
-      priceHigh: this.ethereumData()[this.step].priceHigh,
-      priceLow: this.ethereumData()[this.step].priceLow,
-    };
-
-    let currentIndex = Math.trunc(this.lastData.priceLow / 100);
-    let objetiveIndex = Math.trunc(newData.priceLow / 100);
-
-    //Compro si baja
-    while (currentIndex > objetiveIndex) {
-      const buyPrice = currentIndex * 100;
-      const order = this.orders().find((x) => x.buyPrice === buyPrice);
-      if (order?.isPending) {
-        this.buyCrypto(buyPrice);
+    const newData = this.getLastDataFromStep(this.step);
+    const sucesionOfMultiples = this.getSucesionOfPrices(newData);
+    let lastPrice = newData.priceOpen;
+    for (const element of sucesionOfMultiples) {
+      const orderBuy = this.orders().find((x) => x.buyPrice === element);
+      if (
+        orderBuy?.status === 'pendingToBuy' &&
+        orderBuy?.buyPrice === element
+      ) {
+        this.buyCrypto(element);
       }
-      currentIndex--;
+      const orderSell = this.orders().find((x) => x.sellPrice === element);
+      if (element > lastPrice) {
+        if (
+          orderSell?.status === 'pendingToSell' &&
+          orderSell?.sellPrice === element
+        ) {
+          this.sellCrypto(element);
+        }
+      }
     }
-
-    //Compro si sube, pero tambien vendo si sube
-    currentIndex = Math.trunc(this.lastData.priceLow / 100);
-    objetiveIndex = Math.trunc(newData.priceLow / 100);
-
-    while (currentIndex < objetiveIndex) {
-      const buyPrice = (currentIndex + 1) * 100;
-      //Verifico si se puede comprar algo
-      const order = this.orders().find((x) => x.buyPrice === buyPrice);
-      if (order?.isPending) {
-        this.buyCrypto(buyPrice);
-      }
-      //Verifico si se puede vender algo
-      const pendingSellOrders = this.pendingSellOrders().find(
-        (x) => x.price === buyPrice
-      );
-      if (pendingSellOrders) {
-        this.sellCrypto(buyPrice);
-      }
-      currentIndex++;
-    }
-
-    //Math.trunc(this.lastData.priceLow / 100)
-    //eg: 2267 -> 22
-    //Math.trunc(newData.priceLow / 100) * 100 ;
-    //eg: 2449 -> 24
-    //Si el precio bajo mas de lo que estaba
+    this.lastData.set(newData);
+    this.orders.update((orders) =>
+      this.sortOrdersByProximity(orders, this.lastData().priceClose)
+    );
   }
   buyCrypto(price: number) {
-    this.money -= 100;
-    const operation: Operation = {
-      action: 'buy',
-      price: price,
-    };
-    this.stock.update((x) => [...x, operation]);
-    this.changeOrderStatus(price, false);
-    const sellOrder: SellOrder = {
-      price: price + 100,
-    };
-    this.pendingSellOrders.update((x) => [...x, sellOrder]);
+    this.money -= this.intervalForBuy;
+    this.changeOrderStatusByBuyPrice(price, 'pendingToSell');
   }
   sellCrypto(price: number) {
-    this.pendingSellOrders.update((pendingSellOrders) =>
-      pendingSellOrders.filter((order) => order.price !== price)
-    );
-    this.stock.update((stock) =>
-      stock.filter((order) => order.price !== price)
-    );
-    this.changeOrderStatus(price - 100, true);
-    //Calculo el revenue y lo guardo
-    const profit = (100 / (price - 100)) * price - 100;
-    const newClosedPosition: ClosedPosition = {
-      buyPrice: price - 100,
-      sellPrice: price,
-      profit: profit,
-    };
-    this.historicalClosedPosition.update((x) => [...x, newClosedPosition]);
+    const order = this.orders().find((x) => x.sellPrice === price);
+    debugger;
+    if (order) {
+      this.changeOrderStatusByBuyPrice(price, 'pendingToBuy');
+      const profit = (this.intervalForBuy / order.buyPrice) * order.sellPrice;
+      const closedOrder = {
+        buyPrice: order.buyPrice,
+        sellPrice: order.sellPrice,
+        profit: profit - this.intervalForBuy,
+      };
+      this.money += profit;
+      this.historicalClosedPosition.update((existingClosedPositions) => [
+        ...existingClosedPositions,
+        closedOrder,
+      ]);
+    }
   }
-  changeOrderStatus(price: number, newState: boolean) {
+  changeOrderStatusByBuyPrice(price: number, newState: OrderStatus) {
     this.orders.update((orders) =>
       orders.map((order) =>
-        order.buyPrice === price ? { ...order, isPending: newState } : order
+        order.buyPrice === price ? { ...order, status: newState } : order
       )
     );
+  }
+  changeOrderStatusBySellPrice(price: number, newState: OrderStatus) {
+    this.orders.update((orders) =>
+      orders.map((order) =>
+        order.sellPrice === price ? { ...order, status: newState } : order
+      )
+    );
+  }
+  getLastDataFromStep(step: number) {
+    return {
+      priceHigh: this.ethereumData()[step].priceHigh,
+      priceLow: this.ethereumData()[step].priceLow,
+      priceClose: this.ethereumData()[step].priceClose,
+      priceOpen: this.ethereumData()[step].priceOpen,
+      timeHigh: this.ethereumData()[step].timeHigh,
+      timeLow: this.ethereumData()[step].timeLow,
+      timeOpen: this.ethereumData()[step].timeOpen,
+    };
+  }
+  getMultiplesOf100Between(num1: number, num2: number) {
+    const multiples = [];
+
+    // Ordenar los números, pero guardar el orden original
+    const sorted = [num1, num2].sort((a, b) => a - b);
+    const reverseOrder = num1 > num2; // ¿El orden original era inverso?
+
+    // Encontrar el primer múltiplo de this.intervalForBuy mayor o igual que el mínimo
+    let currentMultiple =
+      Math.ceil(sorted[0] / this.intervalForBuy) * this.intervalForBuy;
+
+    // Generar los múltiplos hasta que sean mayores que el máximo
+    while (currentMultiple <= sorted[1]) {
+      multiples.push(currentMultiple);
+      currentMultiple += this.intervalForBuy;
+    }
+
+    // Revertir el array si el orden original era inverso
+    return reverseOrder ? multiples.reverse() : multiples;
+  }
+  getSucesionOfPrices(newData: LastData) {
+    const [first, second, third] =
+      newData.timeHigh < newData.timeLow
+        ? [
+            newData.priceOpen,
+            newData.priceHigh,
+            newData.priceLow,
+            newData.priceClose,
+          ]
+        : [
+            newData.priceOpen,
+            newData.priceLow,
+            newData.priceHigh,
+            newData.priceClose,
+          ];
+
+    return [
+      ...this.getMultiplesOf100Between(first, second),
+      ...this.getMultiplesOf100Between(second, third),
+      ...this.getMultiplesOf100Between(third, newData.priceClose),
+    ];
   }
 }
